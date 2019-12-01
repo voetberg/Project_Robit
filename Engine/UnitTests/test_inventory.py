6935c3ab3a8dc0583f597d7146315ca1b5ffc846
voetberg <magpie127@gmail.com>
2019-12-01 16:43:10 -0600

    Expected behavior for inventory

--- Engine/UnitTests/test_inventory.py
+++ Engine/UnitTests/test_inventory.py
@@ -7,16 +7,33 @@ class TestInventory(TestCase):
         self.inventory = Inventory
 
     def test_add_item(self):
-        self.inventory.add_item(inventory)
+        self.inventory.add_item("trash")
+        self.assertEqual(1,self.inventory.size)
 
     def test_remove_item(self):
-        pass
+        self.inventory.add_item("trash")
+        self.inventory.remove_item("trash")
+        self.assertEqual(0,self.inventory.size)
 
     def test_exceed_inventory_space(self):
-        pass
+        for _ in range(0,self.inventory.max_item_number):
+            self.inventory.add_item("trash")
+        expected = "Inventory Full"
+        self.assertEqual(expected,self.inventory.add_item("trash"))
 
     def test_display_all_names(self):
-        pass
+        self.inventory.add_item("trash")
+        expected = ["Trash"]
+        self.assertEqual(expected,self.inventory.item_names)
 
     def test_give_item_discription(self):
-        pass
+        self.inventory.add_item("trash")
+        expected = ["It's Trash"]
+        self.assertEqual(expected,self.inventory.item_descriptions)
+
+    def test_give_item_stats(self):
+        self.inventory.add_item("trash")
+        expected = [{
+            
+        }]
+        self.assertEqual(expected,self.inventory.item_stats)

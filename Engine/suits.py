3d7f12bc93fb2e411224c7d9063747753bb4435e
voetberg <magpie127@gmail.com>
2019-12-03 21:34:37 -0600

    I'm very sneepy

--- Engine/suits.py
+++ Engine/suits.py
@@ -1,5 +1,8 @@
 from project_robit.Engine.cards import Cards
 
-class Suits(Cards):
+
+class Suits:
     def __init__(self, name="Name"):
         self.name = name
+
+        #TODO Everything. Everything. I'm very sleepy.

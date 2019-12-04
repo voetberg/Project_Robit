4c10f28dfefd89752c5321a6d9616efc4f931a30
voetberg <magpie127@gmail.com>
2019-12-03 21:33:53 -0600

    refact to have a deck inhertent cards

--- Engine/cards.py
+++ Engine/cards.py
@@ -1,6 +1,25 @@
-class Cards:
-    def __init__(self):
-        pass
+from .resource_accessor import ResourceAccessor
 
-    def use_card(self):
-        pass
+
+class Cards:
+    def __init__(self,suit):
+        resources = ResourceAccessor("cards")
+        self.deck = resources[suit]
+
+    def get_card(self,card_number):
+        return Card(self.deck[card_number])
+
+    def drop_card(self,card_number):
+        self.deck.pop(card_number)
+
+
+class Card:
+    def __init__(self,card):
+        self.attack = card['attack']
+        self.heal = card['heal']
+        self.defense_buff = card['defense_buff']
+        self.attack_buff = card['attack_buff']
+
+
+
+

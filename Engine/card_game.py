7e12f37863917d501b68a16918cee1ddd0bab9aa
voetberg <magpie127@gmail.com>
2020-02-06 15:31:09 -0600

    run game and check win conditions

--- Engine/card_game.py
+++ Engine/card_game.py
@@ -1,15 +1,49 @@
-class CardGame():
-    def init(self):
+from .player import Player
+
+class CardGame:
+    def __init__(self, day, winning_threshold):
+        self.juniper = Player(day, "Juniper")
+        self.citrus = Player(day, "Citrus")
+
+    def draw_card(self, player):
         pass
 
-    def draw_card(self):
+    def play_card(self, player):
         pass
 
-    def play_card(self):
+    def discard_card(self, player):
         pass
 
-    def discard_card(self):
+    def reveal_card(self, player):
         pass
 
-    def win_state(self):
+    def game_finished(self):
+        state = 1
+        state *= self.juniper.hp <= 0
+        state *= self.juniper.hp >= 12
+        state *= self.citrus.hp <= 0
+        state *= self.citrus.hp >= 12
+        return state
+
+    def play_turn(self):
+
         pass
+
+    def play_game(self):
+        for _ in range(10):
+            if self.game_finished() == 1:
+                pass
+            else:
+                self.play_turn()
+        # TODO Optimize bc this is far too slow
+        if self.juniper.hp > self.citrus.hp:
+            self.winner = self.juniper
+        elif self.juniper < self.citrus:
+            self.winner = self.citrus
+        else:
+            self.winner.name = None
+
+        return self.winner.name
+
+
+
--- Engine/player.py
+++ Engine/player.py
@@ -2,7 +2,8 @@ from . resource_access import Resource_Manager
 
 class Player:
     def __init__(self, day='0', character=''):
+        self.name = character
         self.hp = 6
         self.deck = Resource_Manager(['Cards',day, character]) # TODO Make sure this is a good naming method
         self.won = False
-        
+

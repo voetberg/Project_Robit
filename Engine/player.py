f6e1fb60f92b7d1a23761ad018801484353ce4d7
voetberg <magpie127@gmail.com>
2020-02-06 14:04:51 -0600

    basic player class

---
+++ Engine/player.py
@@ -0,0 +1,8 @@
+from . resource_access import Resource_Manager
+
+class Player:
+    def __init__(self, day='0', character=''):
+        self.hp = 6
+        self.deck = Resource_Manager(['Cards',day, character]) # TODO Make sure this is a good naming method
+        self.won = False
+        

e2210aec7a858419132c22ae358fb8c210d255e9
voetberg <magpie127@gmail.com>
2020-02-06 13:30:25 -0600

    stub functions

---
+++ Engine/card_game.py
@@ -0,0 +1,15 @@
+class CardGame():
+    def init(self):
+        pass
+
+    def draw_card(self):
+        pass
+
+    def play_card(self):
+        pass
+
+    def discard_card(self):
+        pass
+
+    def win_state(self):
+        pass
